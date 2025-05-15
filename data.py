460376846d34c27418fa29afef79d89e2e4740eb
geeshan33 <100735732+geeshan33@users.noreply.github.com>
2025-05-15 01:00:56 -0400

    Second change

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
 # File with constants
+URBAN_ROUTES_URL = 'https://cnt-bade486c-b26c-40c3-9d4d-0e012234230f.containerhub.tripleten-services.com'
+ADDRESS_FROM = 	'East 2nd Street, 601'
+ADDRESS_TO = 	'1300 1st St'
+PHONE_NUMBER = 	'+1 123 123 12 12'
+CARD_NUMBER = 	'1234 5678 9100'
+CARD_CODE = 	'1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
--- main.py
+++ main.py
@@ -1,2 +1,55 @@
+from helpers import is_url_reachable
+from data import URBAN_ROUTES_URL
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        if is_url_reachable(URBAN_ROUTES_URL):
+            print("Connected to the Urban Routes server")
+        else:
+            print("Cannot connect to Urban Routes. Check the server is on and still running")
+
+    def test_set_route(self):
+        # Add in S8
+        print("function created for set route")
+        pass
+
+    def test_select_plan(self):
+            # Add in S8
+            print("function created for set route")
+            pass
+
+    def test_fill_phone_number(self):
+                # Add in S8
+                print("function created for set route")
+                pass
+
+    def test_fill_card(self):
+                    # Add in S8
+                    print("function created for set route")
+                    pass
+
+    def test_comment_for_driver(self):
+                        # Add in S8
+                        print("function created for set route")
+                        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+                            # Add in S8
+                            print("function created for set route")
+                            pass
+
+    def test_order_2_ice_creams(self):
+                                # Add in S8
+                                print("function created for set route")
+                                pass
+
+    def test_car_search_model_appears(self):
+                                    # Add in S8
+                                    print("function created for set route")
+                                    pass
+
+    def test_order_2_ice_creams1(self):
+        for _ in range(2):  # Loop runs twice
+            # Add in S8
+            pass
