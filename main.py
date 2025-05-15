88a060d9e66eff8cc4347ad26109090926e77233
geeshan33 <100735732+geeshan33@users.noreply.github.com>
2025-05-15 11:06:09 -0400

    Third Change

--- main.py
+++ main.py
@@ -15,41 +15,43 @@ class TestUrbanRoutes:
         pass
 
     def test_select_plan(self):
-            # Add in S8
-            print("function created for set route")
-            pass
+        # Add in S8
+        print("function created for set route")
+        pass
 
     def test_fill_phone_number(self):
-                # Add in S8
-                print("function created for set route")
-                pass
+        # Add in S8
+        print("function created for set route")
+        pass
 
     def test_fill_card(self):
-                    # Add in S8
-                    print("function created for set route")
-                    pass
+        # Add in S8
+        print("function created for set route")
+        pass
 
     def test_comment_for_driver(self):
-                        # Add in S8
-                        print("function created for set route")
-                        pass
+        # Add in S8
+        print("function created for set route")
+        pass
 
     def test_order_blanket_and_handkerchiefs(self):
-                            # Add in S8
-                            print("function created for set route")
-                            pass
+        # Add in S8
+        print("function created for set route")
+        pass
 
     def test_order_2_ice_creams(self):
-                                # Add in S8
-                                print("function created for set route")
-                                pass
+        # Add in S8
+        print("function created for set route")
+        pass
 
     def test_car_search_model_appears(self):
-                                    # Add in S8
-                                    print("function created for set route")
-                                    pass
+        # Add in S8
+        print("function created for set route")
+        pass
 
     def test_order_2_ice_creams1(self):
-        for _ in range(2):  # Loop runs twice
-            # Add in S8
-            pass
+        number_of_ice_creams = 2
+        for count in range(number_of_ice_creams):
+            print("function created for set route")
+        # todo S8
+        pass
