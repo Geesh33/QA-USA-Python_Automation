55a4b04929b071db8af74e56523dd98009f9c179
geeshan33 <100735732+geeshan33@users.noreply.github.com>
2025-06-10 17:42:54 -0400

    fifth change

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 # File with constants
-URBAN_ROUTES_URL = 'https://cnt-002fea6c-3fd6-4f10-874b-7e688ce3e035.containerhub.tripleten-services.com'
+URBAN_ROUTES_URL = ' https://cnt-40bd7f65-a3ef-4ec2-943d-9ddb56178d8f.containerhub.tripleten-services.com'
 ADDRESS_FROM = 	'East 2nd Street, 601'
 ADDRESS_TO = 	'1300 1st St'
 PHONE_NUMBER = 	'+1 123 123 12 12'
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -22,73 +23,96 @@ class TestUrbanRoutes:
         else:
             print("Cannot connect to Urban Routes. Check the server is on and still running")
 
+
     def test_set_route(self):
-        self.driver.get(URBAN_ROUTES_URL)
+        self.driver.get(data.URBAN_ROUTES_URL)
+        time.sleep(1)
+
         routes_page = UrbanRoutesPage(self.driver)
         routes_page.set_route(data.ADDRESS_FROM, data.ADDRESS_TO)
+        time.sleep(1)
+
         assert routes_page.get_from() == data.ADDRESS_FROM
         assert routes_page.get_to() == data.ADDRESS_TO
 
     def test_select_plan(self):
-       self.driver.get(URBAN_ROUTES_URL)
-       routes_page = UrbanRoutesPage(self.driver)
-       routes_page.set_route(data.ADDRESS_FROM, data.ADDRESS_TO)
-       routes_page.select_supportive_plan()
-       assert routes_page.get_current_selected_plan() == 'Supportive'
+        self.driver.get(data.URBAN_ROUTES_URL)
+        time.sleep(1)
 
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.set_route(data.ADDRESS_FROM, data.ADDRESS_TO)
+        time.sleep(1)
+
+        routes_page.select_supportive_plan()
+        time.sleep(1)
+
+        assert routes_page.get_current_selected_plan() == 'Supportive'
 
     def test_fill_phone_number(self):
         self.driver.get(data.URBAN_ROUTES_URL)
+        time.sleep(1)
+
         routes_page = UrbanRoutesPage(self.driver)
         routes_page.set_route(data.ADDRESS_FROM, data.ADDRESS_TO)
+        time.sleep(1)
+
         routes_page.enter_phone_number(data.PHONE_NUMBER)
+        time.sleep(1)
+
         phone_value = self.driver.find_element(By.ID, 'phone').get_attribute('value')
         assert phone_value == data.PHONE_NUMBER
 
     def test_fill_card(self):
-        self.driver.get(URBAN_ROUTES_URL)
+        self.driver.get(data.URBAN_ROUTES_URL)
+        time.sleep(1)
+
         routes_page = UrbanRoutesPage(self.driver)
         routes_page.enter_card_details(data.CARD_NUMBER, data.EXPIRY_DATE, data.CVV)
+        time.sleep(1)
+
         card_value = self.driver.find_element(By.ID, 'card-number').get_attribute('value')
         assert card_value.endswith("1111")
 
     def test_comment_for_driver(self):
-        self.driver.get(URBAN_ROUTES_URL)
+        self.driver.get(data.URBAN_ROUTES_URL)
+        time.sleep(1)
+
         routes_page = UrbanRoutesPage(self.driver)
         routes_page.add_driver_comment(data.DRIVER_COMMENT)
+        time.sleep(1)
+
         comment_value = self.driver.find_element(By.ID, 'driver-comment').get_attribute('value')
         assert comment_value == data.DRIVER_COMMENT
 
     def test_order_blanket_and_handkerchiefs(self):
-        self.driver.get(URBAN_ROUTES_URL)
+        self.driver.get(data.URBAN_ROUTES_URL)
+        time.sleep(1)
+
         routes_page = UrbanRoutesPage(self.driver)
         routes_page.order_blanket_and_handkerchiefs()
-        # Optionally assert some visible confirmation
+        time.sleep(1)
+
         blanket_ordered = self.driver.find_element(By.ID, 'blanket-confirm').is_displayed()
         handkerchief_ordered = self.driver.find_element(By.ID, 'handkerchief-confirm').is_displayed()
         assert blanket_ordered and handkerchief_ordered
 
     def test_order_2_ice_creams(self):
-        self.driver.get(URBAN_ROUTES_URL)
+        self.driver.get(data.URBAN_ROUTES_URL)
+        time.sleep(1)
+
         routes_page = UrbanRoutesPage(self.driver)
         routes_page.order_multiple_ice_creams(count=2)
-        # Assert visible confirmation exists
+        time.sleep(1)
+
         confirmations = self.driver.find_elements(By.CLASS_NAME, 'ice-cream-confirmed')
         assert len(confirmations) >= 2
 
     def test_car_search_model_appears(self):
-        self.driver.get(URBAN_ROUTES_URL)
+        self.driver.get(data.URBAN_ROUTES_URL)
+        time.sleep(1)
+
         routes_page = UrbanRoutesPage(self.driver)
         assert routes_page.car_model_is_visible()
-
-
-    def test_order_2_ice_creams1(self):
-        self.driver.get(URBAN_ROUTES_URL)
-        routes_page = UrbanRoutesPage(self.driver)
-        routes_page.order_multiple_ice_creams(count=2)
-        confirmations = self.driver.find_elements(By.CLASS_NAME, 'ice-cream-confirmed')
-        assert len(confirmations) >= 2
-
 
     @classmethod
     def teardown_class(cls):
